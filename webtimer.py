74d457fbd114d4c9542163529b3beff174859182
chris <chris@coronis>
2014-03-30 18:41:42 -0400

    Updated header.

--- webtimer.py
+++ webtimer.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python
 """
-2014.3.30 CKS
+Copyright (C) 2014  Chris Spencer (chrisspen at gmail dot com)
+
 Measures download times for all resources on a webpage.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 """
+
+VERSION = (0, 0, 1)
+__version__ = '.'.join(map(str, VERSION))
+
 import os
 import sys
 import re
